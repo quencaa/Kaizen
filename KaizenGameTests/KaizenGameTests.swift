b6a24bbee35181deb9510bc513e24523b4961b20
Gustavo Teixeira <guteixeira@deloitte.pt>
2024-09-24 18:11:09 +0100

    Initial Commit

---
+++ KaizenGameTests/KaizenGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  KaizenGameTests.swift
+//  KaizenGameTests
+//
+//  Created by Gustavo Quenca on 24/09/2024.
+//
+
+import Testing
+@testable import KaizenGame
+
+struct KaizenGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
